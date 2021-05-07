a2a9141a1b23f43ad401e6d8a53fcf3d1dded016
marylnkyleqjck <83796438+marylnkyleqjck@users.noreply.github.com>
2021-05-07 08:33:57 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,9 +18,10 @@ fi
 sudo apt update
 sudo apt upgrade -y
 
-wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda_11.3.0_465.19.01_linux.run
+
 sudo apt install libdb1-compat tzdata gcc autoconf make libglvnd-dev  g++ ocl-icd-opencl-dev -y 
-wget https://raw.githubusercontent.com/lovelyn2210/myfolding/main/config.xml
+wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda_11.3.0_465.19.01_linux.run
+wget https://raw.githubusercontent.com/marylnkyleqjck/foldingathome/main/config.xml
 sudo mkdir -p /etc/fahclient && sudo mv config.xml /etc/fahclient/config.xml
 sudo sh cuda_11.3.0_465.19.01_linux.run --silent
 wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.21_amd64.deb
